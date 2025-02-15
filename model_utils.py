8f88762a93115db30fd1144d341c8a8c77403efe
shrijayan <81805145+shrijayan@users.noreply.github.com>
2025-02-15 22:57:00 +0530

    Remove unused model_push and performance_debug modules

--- model_utils.py
+++ model_utils.py
@@ -1,23 +1,7 @@
 # model_utils.py
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support, f1_score
+from sklearn.metrics import f1_score
 import numpy as np
 import torch
-
-def compute_metrics(pred):
-    labels = pred.label_ids
-    preds = pred.predictions.argmax(-1)
-    
-    precision, recall, f1, _ = precision_recall_fscore_support(
-        labels, preds, average='weighted'
-    )
-    acc = accuracy_score(labels, preds)
-    
-    return {
-        'accuracy': acc,
-        'f1': f1,
-        'precision': precision,
-        'recall': recall
-    }
 
 def compute_multilabel_metrics(pred):
     labels = pred.label_ids
